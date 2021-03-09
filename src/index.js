ce0994e7035ab0ef2e03a70694b0d73f1a9b2ae4
ValeronChareuka <vchareuka8@gmail.com>
2021-03-09 18:27:54 +0300

    feat: - add logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = bracketsConfig.map(b => b.join(""));
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.includes(brackets[i])) {
+            str = str.replace(brackets[i], "");
+            i = -1;
+        }
+    }
+    return str ? false : true;
+};
